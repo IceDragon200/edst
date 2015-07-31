442457464e2675ce0cac90bcf3335f95e8b4b2e4
Corey Powell <mistdragon100@gmail.com>
2015-07-31 14:51:11 -0500

    Updated renderers_spec to include #render_file

--- spec/edst/renderers/renderers_spec.rb
+++ spec/edst/renderers/renderers_spec.rb
@@ -1,28 +1,39 @@
 require 'spec_helper'
 require 'edst/renderers'
 require 'tempfile'
+require 'tmpdir'
 
 # TODO: probably check the output
 
 shared_examples_for "a renderer" do
-  it 'renders a String stream to a String' do
-    dest = ''
-    src = File.read(fixture_pathname('edstspec.edst'))
-    subject.render_stream_to src, dest
-  end
-
-  it 'renders a File stream to a String' do
-    dest = ''
-    File.open(fixture_pathname('edstspec.edst'), 'r') do |src|
+  context '#render_stream_to' do
+    it 'renders a String stream to a String' do
+      dest = ''
+      src = File.read(fixture_pathname('edstspec.edst'))
       subject.render_stream_to src, dest
+    end
+
+    it 'renders a File stream to a String' do
+      dest = ''
+      File.open(fixture_pathname('edstspec.edst'), 'r') do |src|
+        subject.render_stream_to src, dest
+      end
+    end
+
+    it 'renders a File stream to a File' do
+      tmp = Tempfile.new('edst-testfile.edst')
+      File.open(tmp.path, 'w') do |dest|
+        File.open(fixture_pathname('edstspec.edst'), 'r') do |src|
+          subject.render_stream_to src, dest
+        end
+      end
     end
   end
 
-  it 'renders a File stream to a File' do
-    tmp = Tempfile.new('edst-testfile.edst')
-    File.open(tmp.path, 'w') do |dest|
-      File.open(fixture_pathname('edstspec.edst'), 'r') do |src|
-        subject.render_stream_to src, dest
+  context '#render_file' do
+    it 'renders a file to another file' do
+      Dir.mktmpdir('edst-testdir') do |dir|
+        subject.render_file fixture_pathname('edstspec.edst'), directory: dir
       end
     end
   end
