d3eb0606d8d5b7be59b608dcc6dda1b016fea667
Corey Powell <mistdragon100@gmail.com>
2015-08-19 11:51:16 -0500

    Bumped version to 0.29.0

--- lib/edst/version.rb
+++ lib/edst/version.rb
@@ -1,7 +1,7 @@
 module EDST
   # Le Version module, what more do you need to know.
   module Version
-    MAJOR, MINOR, TEENY, PATCH = 0, 28, 1, nil
+    MAJOR, MINOR, TEENY, PATCH = 0, 29, 0, nil
     STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.').freeze
   end
   VERSION = Version::STRING
