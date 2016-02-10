f7c720f77723701f12571e5edb5238653a68397b
Corey Powell <mistdragon100@gmail.com>
2016-02-09 19:52:30 -0500

    Updated ostruct core extentions

--- lib/edst/core_ext/ostruct.rb
+++ lib/edst/core_ext/ostruct.rb
@@ -1,11 +1,25 @@
 require 'ostruct'
 
 class OpenStruct
-  def self.conj(a, b)
-    new(a).tap do |c|
+  # For merging any object that responds to each_pair into the given OpenStruct
+  #
+  # @param [OpenStruct] c  target
+  # @param [Array<#each_pair>] args
+  # @return [OpenStruct]
+  def self.conj!(c, *args)
+    args.each do |b|
       b.each_pair do |k, v|
         c[k] = v
       end
     end
+    c
+  end
+
+  # For merging any object that responds to each_pair into a new OpenStruct
+  #
+  # @param [Array<#each_pair>] args
+  # @return [OpenStruct]
+  def self.conj(*args)
+    conj!(new, *args)
   end
 end
