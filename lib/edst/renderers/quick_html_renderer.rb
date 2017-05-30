8cbb4366c551c706ae9ab0806ebb356f8a050de8
Corey Powell <mistdragon100@gmail.com>
2017-05-30 16:28:32 -0500

    Fixed generic renderer

--- lib/edst/renderers/quick_html_renderer.rb
+++ lib/edst/renderers/quick_html_renderer.rb
@@ -1,11 +1,11 @@
-require 'slim'
+require 'erubis'
 require 'edst/renderers/base_renderer'
 
 module EDST
   # EDST's generic HTML renderer
   class QuickHtmlRenderer < BaseRenderer
     def initialize
-      super template_name: 'views/generic.html.slim'
+      super template_name: 'views/generic.html.erb'
     end
 
     register 'html'
